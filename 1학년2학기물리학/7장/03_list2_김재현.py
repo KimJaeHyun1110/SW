08f482dcb9280baaedc5aeb2e9391e80baf99a52
KimJaeHyun1110 <127718183+KimJaeHyun1110@users.noreply.github.com>
2023-10-18 12:00:31 +0900

    10월 18일 실습

---
+++ 1학년2학기물리학/7장/03_list2_김재현.py
@@ -0,0 +1,15 @@
+'''
+작성일 : 10월 18일
+학과 : 컴퓨터공학
+학번 : 202395010
+이름 : 김재현
+설명 : 7장 리스트
+'''
+#리스트생성
+num_list=[1,2,3,4,5,6,7,8,9]
+print("num_list의 길이 : ",len(num_list))
+print("num_list의 최대값 : ",max(num_list))
+print("num_list의 최소값 : ",min(num_list))
+print("num_list의 항목의 합계 : ",sum(num_list))
+print("num_list의 항목의 평균 : ",sum(num_list)/len(num_list))
+print("num_list의 항목 중 0이 아닌 원소가 있는가> : ",any(num_list))
